40cd475667cd285427044217b007fa021d28a95f
Hugo Ribeira <hugoribeira@gmail.com>
2020-11-28 09:48:38 +0000

    Use the same algorithm as the ladder for engine runs

--- src/flight_computer/calculators/engine_in_use.ts
+++ src/flight_computer/calculators/engine_in_use.ts
@@ -9,7 +9,7 @@ const ENGINE_OFF = dimensionless(0);
 const ENGINE_ON = dimensionless(1);
 
 export default class EngineInUse extends Calculator {
-  private window = new MovingWindow(seconds(30), dimensionless.unit);
+  private window = new MovingWindow(seconds(12), dimensionless.unit);
   private threshold: number;
 
   constructor(threshold: number) {
@@ -32,7 +32,7 @@ export default class EngineInUse extends Calculator {
   }
 
   getValue() {
-    if (this.window.average().value > this.threshold / 2) {
+    if (this.window.min().value > this.threshold) {
       return ENGINE_ON;
     } else {
       return ENGINE_OFF;
